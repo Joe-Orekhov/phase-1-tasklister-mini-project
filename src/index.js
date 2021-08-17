440e93d99b7be2950e8839af933cae7d044818a4
joesephorekhov <josephorekhov@gmail.com>
2021-08-17 10:03:41 -0600

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+const submit = document.querySelector('form');
+submit.addEventListener("submit", (e) => {
+  e.preventDefault()
+  createToDo()
+  submit.reset()
+})
+
+function createToDo(){
+      
+    const input = document.getElementById("new-task-description");
+    const aNewTask = document.createElement('li');
+    const deleteBttn = document.createElement('button');
+
+    aNewTask.textContent = input.value;
+    document.getElementById('tasks').appendChild(aNewTask); 
+  
+
+    deleteBttn.textContent = 'Bye';
+    aNewTask.appendChild(deleteBttn);
+    deleteBttn.addEventListener('click', () => aNewTask.remove())
+}    
+
+})
